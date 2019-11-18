bd07737d5a12f679554f236a933bb928b6d25e0b
brainsqueeze <dave.f.hollander@gmail.com>
2019-11-18 13:11:35 -0500

    For better setup tools

---
+++ text2vec/__init__.py
@@ -0,0 +1,11 @@
+from . import models
+from . import preprocessing
+from . import optimizer_tools
+from . import training_tools
+
+__all__ = [
+    'models',
+    'preprocessing',
+    'optimizer_tools',
+    'training_tools'
+]
