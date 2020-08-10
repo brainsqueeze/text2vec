e0dbb91949ce5326a9219994b00e3d0f12d7697a
Dave <dave.f.hollander@gmail.com>
2020-08-10 15:50:26 -0400

    Increment version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="text2vec",
-    version="0.4.1",
+    version="0.4.2",
     description="Building blocks for text vectorization and embedding",
     author="Dave Hollander",
     author_url="https://github.com/brainsqueeze",
