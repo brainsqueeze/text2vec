a451e77255f95316d8b90f62403d78f0b0c3443d
brainsqueeze <dave.f.hollander@gmail.com>
2021-09-28 11:03:38 -0400

    SubTokenFinderMask -> SubStringFinderMask

--- text2vec/models/components/strings.py
+++ text2vec/models/components/strings.py
@@ -3,25 +3,20 @@ import tensorflow as tf
 from text2vec.models import Tokenizer
 
 
-class SubTokenFinderMask(tf.keras.layers.Layer):
+class SubStringFinderMask(tf.keras.layers.Layer):
     """Performs substring masking based on whether the substring is found in the input text
-    either in part or in entirety. The substring search implementation is simple and is performed by tokenizing
-    the input text with some specified separator. If you require more complex searching, then it is advised that
-    the input text be pre-processed with a more robust tokenizer, such as BPE or WordPiece.
+    in its entirety. This returns a ragged boolean tensor with the same ragged shape as input substrings.
 
     Parameters
     ----------
     sep : str, optional
         The token to split the incoming strings by, by default ' '
-    mode : str, optional
-        The final reduction mode. Setting `mode` to 'all' will mask the entire substring if any subtokens are not found,
-        by default 'any'
 
     Examples
     --------
     ```python
     import tensorflow as tf
-    from text2vec.models.components.strings import SubTokenFinderMask
+    from text2vec.models.components.strings import SubStringFinderMask
 
     text = tf.constant([
         "Sample string.",
@@ -32,84 +27,25 @@ class SubTokenFinderMask(tf.keras.layers.Layer):
         ["string"],
         ["this", "apple", "example"]
     ])
-    mask_agent = SubTokenFinderMask()
+    mask_agent = SubStringFinderMask()
     mask_agent(text, substrings)
     ```
     """
 
-    def __init__(self, sep: str = ' ', mode: str = 'any'):
+    def __init__(self, sep: str = ' '):
         super().__init__()
-
         self.tokenizer = Tokenizer(sep)
-        if mode == 'all':
-            self.reducer = tf.math.reduce_all
-        else:
-            self.reducer = tf.math.reduce_any
+        self.match = tf.keras.layers.Lambda(lambda x: tf.strings.regex_full_match(input=x[0], pattern=x[1]))
 
-    @staticmethod
-    def find_token(text_tokens: tf.Tensor, token) -> tf.Tensor:
-        """Finds token in the input text as a Tensor element search
+    def find_match(self, texts, substrings):
+        return tf.map_fn(self.match, tf.stack([texts, substrings], axis=1), fn_output_signature=tf.bool)
 
-        Parameters
-        ----------
-        text_tokens : tf.Tensor
-            String to search on, split into sub-tokens
-        token : String-like scalar
-            Token to find
+    def call(self, texts: tf.Tensor, substrings: tf.RaggedTensor) -> tf.RaggedTensor:
+        pre = '.*(\s|^)'
+        post = '(\s|$).*'
 
-        Returns
-        -------
-        tf.Tensor
-            Boolean scalar tensor
-        """
-
-        return tf.math.reduce_any(text_tokens == token)
-
-    def find_token_sequence(self, text_tokens: tf.Tensor, token_sequence: tf.Tensor) -> tf.Tensor:
-        """Finds sequence of tokens in the input text as a Tensor element search
-
-        Parameters
-        ----------
-        text_tokens : tf.Tensor
-            String to search on, split into sub-tokens
-        token_sequence : tf.Tensor
-            Sequence of tokens to find
-
-        Returns
-        -------
-        tf.Tensor
-            Boolean vector tensor
-        """
-
-        return tf.map_fn(lambda x: self.find_token(text_tokens, x), token_sequence, fn_output_signature=tf.bool)
-
-    def find_token_sequence_set(self, text_tokens: tf.Tensor, token_sequence_set: tf.RaggedTensor) -> tf.RaggedTensor:
-        """Finds set of token sequences in the input text
-
-        Parameters
-        ----------
-        text_tokens : tf.Tensor
-            String to search on, split into sub-tokens
-        token_sequence_set : tf.RaggedTensor
-            Set of token sequences
-
-        Returns
-        -------
-        tf.RaggedTensor
-            Ragged boolean tensor
-        """
-
-        return tf.ragged.map_flat_values(lambda x: self.find_token_sequence(text_tokens, x), token_sequence_set)
-
-    def call(self, text: tf.Tensor, substrings: tf.RaggedTensor, token_level_out: bool = True) -> tf.RaggedTensor:
-        combined = tf.concat([tf.expand_dims(self.tokenizer(text), axis=1), self.tokenizer(substrings)], 1)
-
-        token_found = tf.map_fn(
-            lambda x: self.find_token_sequence_set(x[0], x[1:]),
-            combined,
-            fn_output_signature=tf.RaggedTensorSpec(ragged_rank=1, dtype=tf.bool)
+        ragged_texts = tf.RaggedTensor.from_row_lengths(
+            values=tf.repeat(texts, repeats=substrings.row_lengths()),
+            row_lengths=substrings.row_lengths()
         )
-
-        if token_level_out:
-            return token_found
-        return self.reducer(token_found, axis=-1)
+        return tf.ragged.map_flat_values(self.find_match, ragged_texts, tf.strings.join([pre, substrings, post]))
